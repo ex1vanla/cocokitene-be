4978dbaf22a2fa16e28eac3cd4f87443dbae4fae
ttm-kiennv <nguyenkien07032003ns@gmail.com>
2023-11-07 17:57:30 +0700

    refactor: delete_unnecessary_things

--- libs/queries/src/dtos/role.dto.ts
+++ libs/queries/src/dtos/role.dto.ts
@@ -1,9 +1,7 @@
-import { IsEnum, IsNotEmpty, IsNumber } from 'class-validator'
+import { IsNotEmpty, IsNumber } from 'class-validator'
 import { Type } from 'class-transformer'
 import { ApiProperty } from '@nestjs/swagger'
 import { GetAllDto } from '@dtos/base.dto'
-import { ProposalType } from '@shares/constants/proposal.const'
-import { RoleEnum } from '@shares/constants'
 
 export class ActiveUserDto {
     @IsNumber()
@@ -16,22 +14,3 @@ export class ActiveUserDto {
 }
 
 export class GetAllNormalRolesDto extends GetAllDto {}
-
-export class GetAllImportantRoleDto extends GetAllDto {}
-
-export class CreateCompanyRoleDto {
-    @IsEnum(RoleEnum)
-    @ApiProperty({
-        required: true,
-        enum: RoleEnum,
-    })
-    type: ProposalType
-
-    @IsNumber()
-    @IsNotEmpty()
-    @Type(() => Number)
-    @ApiProperty({
-        example: 1,
-    })
-    companyId: number
-}
