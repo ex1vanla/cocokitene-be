df84f864896e5051e20c28b4f18ad32ab98a5f08
ttm-kiennv <nguyenkien07032003ns@gmail.com>
2024-02-20 08:36:21 +0700

    refactor: push data to blockchain

--- apps/cronjob/src/modules/cronjob/cronjob.service.ts
+++ apps/cronjob/src/modules/cronjob/cronjob.service.ts
@@ -34,27 +34,26 @@ export class CronjobService {
         }
     }
 
-    // @Cron(CronExpression.EVERY_SECOND)
-    // async handleAllEndedMeeting() {
-    //     await this.transactionService.handleAllEndedMeeting()
-    // }
-
-    // @Cron(CronExpression.EVERY_30_SECONDS)
-    // async handlePendingTransaction() {
-    //     await this.transactionService.handleCheckTransaction()
-    // }
+    @Cron(CronExpression.EVERY_SECOND)
+    async handleAllEndedMeeting() {
+        await this.transactionService.handleAllEndedMeeting()
+    }
 
     @Cron(CronExpression.EVERY_30_SECONDS)
+    async handlePendingTransaction() {
+        await this.transactionService.handleCheckTransaction()
+    }
+
+    @Cron(CronExpression.EVERY_5_MINUTES)
     async crawlMeetingEvent() {
         const config = await this.getConfigCrawlerByContractType(
             CONTRACT_TYPE.MEETING,
         )
         await this.meetingCrawler.scan(config)
     }
-    //
-    // // @Cron(CronExpression.EVERY_SECOND)
-    // @Cron(CronExpression.EVERY_SECOND)
-    // async handleDataAfterEventSuccessfulCreatedMeeting() {
-    //     await this.transactionService.handleDataAfterEventSuccessfulCreatedMeeting()
-    // }
+
+    @Cron(CronExpression.EVERY_10_MINUTES)
+    async handleDataAfterEventSuccessfulCreatedMeeting() {
+        await this.transactionService.handleDataAfterEventSuccessfulCreatedMeeting()
+    }
 }
