09bb0c395cd586ff76e2814ea712008a1a8a24db
TienNV <tiennv@trithucmoi.co>
2024-03-06 08:31:49 +0700

    feat: fixxxx

--- libs/shares/src/utils/get-ip.ts
+++ libs/shares/src/utils/get-ip.ts
@@ -1,19 +1,19 @@
-import * as os from 'os'
-import configuration from '@shares/config/configuration'
+// import * as os from 'os'
+// import configuration from '@shares/config/configuration'
 
-export const getIpAddress = (): string | null => {
-    const interfaces = os.networkInterfaces()
-    const targetInterface = configuration().network.networkInterface
+// export const getIpAddress = (): string | null => {
+//     const interfaces = os.networkInterfaces()
+//     const targetInterface = configuration().network.networkInterface
 
-    if (interfaces[targetInterface]) {
-        const ipAddress = interfaces[targetInterface]
-            .filter((item) => item.family === 'IPv4' && !item.internal)
-            .map((item) => item.address)
+//     if (interfaces[targetInterface]) {
+//         const ipAddress = interfaces[targetInterface]
+//             .filter((item) => item.family === 'IPv4' && !item.internal)
+//             .map((item) => item.address)
 
-        if (ipAddress.length > 0) {
-            return ipAddress[0]
-        }
-    }
+//         if (ipAddress.length > 0) {
+//             return ipAddress[0]
+//         }
+//     }
 
-    return null
-}
+//     return null
+// }
