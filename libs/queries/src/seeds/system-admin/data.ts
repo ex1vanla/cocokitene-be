72f711c95cde0d4c1b6a097f765654a21009784d
ttm-kiennv <nguyenkien07032003ns@gmail.com>
2024-03-15 18:05:54 +0700

    Revert "refactor: add system admin"
    This reverts commit ce013282896c3936699d7843559fee307592513e.

--- libs/queries/src/seeds/system-admin/data.ts
+++ libs/queries/src/seeds/system-admin/data.ts
@@ -4,7 +4,6 @@ import { SystemAdmin } from '../..'
 export class InertSertSystemAdminDto extends PartialType(SystemAdmin) {}
 
 // nguyentienhuy--->$2b$10$799eKj1aPSb1nJJG8fp9Ge7qIS17GiW.Jbrz3dsmUlA16QU3UQfCe
-//exceedone@2024 ---> $2b$10$JgoWShCh8r.ORSH1MWFUiuVUVf9yFRhBS9nbnzB0qEL.41t1JUkTy
 
 export const systemAdminData: InertSertSystemAdminDto[] = [
     {
@@ -12,14 +11,6 @@ export const systemAdminData: InertSertSystemAdminDto[] = [
         email: 'huynt@trithucmoi.co',
         password:
             '$2b$10$799eKj1aPSb1nJJG8fp9Ge7qIS17GiW.Jbrz3dsmUlA16QU3UQfCe',
-        resetPasswordToken: 'j2GBoY23PgLr8N1iDEz6',
-        resetPasswordExpireTime: new Date('2023-12-31T23:59:59'),
-    },
-    {
-        username: 'snoro',
-        email: 'snoro@exceedone.co.jp',
-        password:
-            '$2b$10$JgoWShCh8r.ORSH1MWFUiuVUVf9yFRhBS9nbnzB0qEL.41t1JUkTy',
         resetPasswordToken: 'j2GBoY23PgLr8N1iDEz6',
         resetPasswordExpireTime: new Date('2023-12-31T23:59:59'),
     },
