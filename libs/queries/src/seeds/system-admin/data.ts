a008361e7b2521f536415b2bd4eee4ebc6539fe5
ttm-kiennv <nguyenkien07032003ns@gmail.com>
2024-03-18 10:59:20 +0700

    feat: add system admin acc

--- libs/queries/src/seeds/system-admin/data.ts
+++ libs/queries/src/seeds/system-admin/data.ts
@@ -3,14 +3,23 @@ import { SystemAdmin } from '../..'
 
 export class InertSertSystemAdminDto extends PartialType(SystemAdmin) {}
 
-// nguyentienhuy--->$2b$10$799eKj1aPSb1nJJG8fp9Ge7qIS17GiW.Jbrz3dsmUlA16QU3UQfCe
+// LeAnhVan --> $2b$10$Rl4P/bblPxAtTWT5VX.wqeB..Lr8a21YiqeW1B.6oGd4hWcwQlemO
+// Exceedone@2024--> $2b$10$7OtV0XT3jR4S6eZ889WV3.HkRbHeZ8tBLLIU0eaIKYh42ZbzmygRW
 
 export const systemAdminData: InertSertSystemAdminDto[] = [
     {
-        username: 'nguyentienhuy',
-        email: 'huynt@trithucmoi.co',
+        username: 'vanla',
+        email: 'vanla@trithucmoi.co',
         password:
-            '$2b$10$799eKj1aPSb1nJJG8fp9Ge7qIS17GiW.Jbrz3dsmUlA16QU3UQfCe',
+            '$2b$10$Rl4P/bblPxAtTWT5VX.wqeB..Lr8a21YiqeW1B.6oGd4hWcwQlemO',
+        resetPasswordToken: 'j2GBoY23PgLr8N1iDEz6',
+        resetPasswordExpireTime: new Date('2023-12-31T23:59:59'),
+    },
+    {
+        username: 'Snoro',
+        email: 'snoro@exceedone.co.jp',
+        password:
+            '$2b$10$7OtV0XT3jR4S6eZ889WV3.HkRbHeZ8tBLLIU0eaIKYh42ZbzmygRW',
         resetPasswordToken: 'j2GBoY23PgLr8N1iDEz6',
         resetPasswordExpireTime: new Date('2023-12-31T23:59:59'),
     },
