36b3a2fc1d3ef96a61393db4182995aa4144de2f
TienNV <tiennv@trithucmoi.co>
2024-07-05 16:31:37 +0700

    fix: create_participant

--- apps/api/src/modules/user-meetings/user-meeting.service.ts
+++ apps/api/src/modules/user-meetings/user-meeting.service.ts
@@ -109,12 +109,26 @@ export class UserMeetingService {
                         UserMeetingStatusEnum.PARTICIPATE,
                     )
                 if (isUserParticipateMeeting) {
-                    await this.createUserMeeting({
-                        userId: usersToAdd,
-                        meetingId: meetingId,
-                        roleMtgId: roleMtgId,
-                        status: UserMeetingStatusEnum.PARTICIPATE,
-                    })
+                    if (roleMtgId === roleMtgShareholderId) {
+                        const quantityOfShareholder =
+                            await this.userService.getQuantityShareByShareholderId(
+                                usersToAdd,
+                            )
+                        await this.createUserMeeting({
+                            userId: usersToAdd,
+                            meetingId: meetingId,
+                            roleMtgId: roleMtgId,
+                            quantityShare: quantityOfShareholder,
+                            status: UserMeetingStatusEnum.PARTICIPATE,
+                        })
+                    } else {
+                        await this.createUserMeeting({
+                            userId: usersToAdd,
+                            meetingId: meetingId,
+                            roleMtgId: roleMtgId,
+                            status: UserMeetingStatusEnum.PARTICIPATE,
+                        })
+                    }
                 } else {
                     if (roleMtgId === roleMtgShareholderId) {
                         const quantityOfShareholder =
